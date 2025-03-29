8ba91f1c00f7e940f0d0b7ddab0eaaca23a6a487
amankum2004 <kumaraman6012@gmail.com>
2025-03-29 13:09:24 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -3,11 +3,9 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
     process.exit(1);
